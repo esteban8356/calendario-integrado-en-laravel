26244e44de4a293f13eb4ff60a44bfba5e4d4929
esteban8356 <99091517+esteban8356@users.noreply.github.com>
2023-01-12 05:53:33 -0500

    Primer comit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('agenda tu cita ') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+
+                    <div id='calendar'></div>
+
+
+
+                    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          
+        });
+        calendar.render();
+      });
+
+    </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
